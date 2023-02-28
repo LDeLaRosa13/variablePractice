9143835cf3547bbe06838451fd0c06059352b4d4
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-02-28 17:27:53 -0500

    Add new meaning to name

--- variables.js
+++ variables.js
@@ -3,3 +3,11 @@ var email = "lstoneman13@gmail.com"
 
 var age = 32
 var phoneNumber = 8168763361
+
+var paid = true
+var happy = true
+
+name = "matt"
+console.log(name)
+console.log(email)
+console.log(age)
